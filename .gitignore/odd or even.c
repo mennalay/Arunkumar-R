11a6965a01510dc9cc6d370b45e872d0dca279dd
mennalay <30343423+mennalay@users.noreply.github.com>
2017-07-23 10:13:26 +0530

    Update odd or even.c

--- .gitignore/odd or even.c
+++ .gitignore/odd or even.c
@@ -1,6 +1,14 @@
-#include<studio.h>
-void main(){
-int a;
-scanf("%d",&a);
-if(a%2==0)
-printf("%d",a);}
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
